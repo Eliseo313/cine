42a73d94cca9f51305bcc7b7a1cd9c82f5b5116e
Eliseo313 <elicarortix58@gmail.com>
2023-04-01 17:05:45 -0600

    validacion requerido/a a asientos

--- asientos.py
+++ asientos.py
@@ -5,9 +5,9 @@ from odoo import models,fields
 class asientos(models.Model):
     _name = 'cine.asientos'
 
-    sala_id = fields.Many2one('cine.salas',string='Sala')
-    fila = fields.Selection([('A','A'),('B','B'),('C','C'),('D','D'),('E','E'),('F','F'),('G','G'),('H','H'),('I','I'),('J','J')],string='Fila')
-    numero = fields.Selection([('1','1'),('2','2'),('3','3'),('4','4'),('5','5'),('6','6'),('7','7'),('8','8'),('9','9'),('10','10')],string='Número')
-    disponible = fields.Selection([('dis','Disponible'),('ocu','Ocupado')],string='Disponibilidad del asiento')
+    sala_id = fields.Many2one('cine.salas',string='Sala',required=True)
+    fila = fields.Selection([('A','A'),('B','B'),('C','C'),('D','D'),('E','E'),('F','F'),('G','G'),('H','H'),('I','I'),('J','J')],string='Fila',required=True)
+    numero = fields.Selection([('1','1'),('2','2'),('3','3'),('4','4'),('5','5'),('6','6'),('7','7'),('8','8'),('9','9'),('10','10')],string='Número',required=True)
+    disponible = fields.Selection([('dis','Disponible'),('ocu','Ocupado')],string='Disponibilidad del asiento',required=True)
 
     _order = 'sala_id,fila,numero,disponible'
