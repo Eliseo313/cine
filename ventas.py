93ca8028de811bab644ca343b7741c9b4c4722b0
Eliseo313 <elicarortix58@gmail.com>
2023-04-02 08:40:23 -0600

    campo "importe" agregado(total a pagar)

--- ventas.py
+++ ventas.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models,fields
+from odoo import models,fields,api
 
 
 class ventas(models.Model):
@@ -8,7 +8,16 @@ class ventas(models.Model):
     empleado_id = fields.Many2one('cine.empleados',string='Empleado',required=True)
     fecha = fields.Date(string='Fecha',required=True)
     name = fields.Char(string='Folio',required=True)
-    cantidad = fields.Integer(string='Cantidad',required=True)
+    cantidad = fields.Integer(string='Cantidad',default=1,required=True)
     producto_id = fields.Many2one('cine.productos',string='Producto comprado',required=True)
+    importe = fields.Char(compute='_cal_importe',string='Total a pagar', readonly=True)
 
-    _order = 'empleado_id,producto_id,fecha,name,cantidad'
+    _order = 'empleado_id,producto_id,fecha,name,cantidad,importe'
+
+    @api.depends('cantidad','producto_id')
+    def _cal_importe(self):
+        for rec in self:
+            if rec.producto_id.precio and rec.cantidad:
+                rec.importe = rec.producto_id.precio * rec.cantidad
+            else:
+                rec.importe = 'No definido'
